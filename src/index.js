0c3f297a60c83250bbe730ad85fbef5ad06ab27f
Sree ganesh <ganuiscool@gmail.com>
2020-06-04 11:01:47 +0530

    Add folder struc redux

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './store/reduser';
-const store = createStore(reducer);
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 const app = (
 	<Provider store={store}>
 		<BrowserRouter>
